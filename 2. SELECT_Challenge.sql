e3dbb182a7f245770af4260ceb6f17dfedf94dee
aakashmt97 <64053786+aakashmt97@users.noreply.github.com>
2020-04-21 10:37:33 +0800

    Add files via upload

---
+++ 2. SELECT_Challenge.sql
@@ -0,0 +1,5 @@
+/* Situation: We need to send out a promotional email to our existing customer.
+              So we need grab First_Name, Last_Name and Emails of all 
+			  existing customers */
+
+SELECT first_name, last_name, email FROM customer;
